b853b3878bebeea6287f2396c441cae90d541ec7
arcbjorn <arky.happ@gmail.com>
2022-10-09 18:49:16 +0400

    feat(App): use Header, Main & Footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import Header from '@/components/Header.vue';
 import Main from '@/components/Main.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
-  <header></header>
-
-  <main>
+  <div class="bg-sky-500 min-h-fit">
+    <Header />
+  </div>
+  <div class="bg-yellow-900 grow">
     <Main />
-  </main>
+  </div>
+  <div class="bg-emerald-500 min-h-fit">
+    <Footer />
+  </div>
 </template>
 
 <style scoped></style>
