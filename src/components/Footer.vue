ef6f52e8b7054efe190051d7d520944f81d3e735
arcbjorn <arky.happ@gmail.com>
2022-10-09 18:29:51 +0400

    feat(components): add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center p-8">
+    <a
+      rel="license"
+      href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB"
+    >
+      <img
+        alt="Creative Commons Licence"
+        style="border-width: 0"
+        src="https://mirrors.creativecommons.org/presskit/buttons/80x15/svg/by-nc-sa.svg"
+      />
+    </a>
+  </div>
+</template>
