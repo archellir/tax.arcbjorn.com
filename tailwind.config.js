66a960c19abee0a6bc601c21441e41bb0c1c6647
arcbjorn <arky.happ@gmail.com>
2022-10-09 18:29:20 +0400

    feat(Tailwind config): add fadeIn anymation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
         'plex-mono': ['IBMPlexMono', 'Courier New', 'Courier', 'monospace'],
         'plex-sans': ['IBMPlexSans', 'sans-serif', 'sans'],
       },
+
+      animation: {
+        appear: 'fadeIn 1s ease-in-out',
+      },
+
+      keyframes: () => ({
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 100 },
+        },
+      }),
     },
   },
   plugins: [],
