5c254c1b40639b068eebcdd3067ba2c1b6d4f0b5
arcbjorn <arky.happ@gmail.com>
2022-10-09 18:48:56 +0400

    feat(Main): add base layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <div class="flex flex-col justify-center items-center h-full">
+    <div class="flex flex-col gap-4">
+      <div class="flex gap-4">
+        <div>DatePicker input</div>
+        <div>ExchangeRate output</div>
+      </div>
+      <div class="flex gap-4">
+        <div>Amount in USD input</div>
+        <div>Amount in Lari output</div>
+      </div>
+      <div>Total tax output</div>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
