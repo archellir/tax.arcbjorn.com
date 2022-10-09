afa2c72a2f67fad74646a6bf5a5bfd689d683131
arcbjorn <arky.happ@gmail.com>
2022-10-09 18:28:34 +0400

    feat: add & use normal, semi-bold & bold fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'plex-mono': ['IBMPlexMono', 'Courier New', 'Courier', 'monospace'],
+        'plex-sans': ['IBMPlexSans', 'sans-serif', 'sans'],
+      },
+    },
   },
   plugins: [],
 };
