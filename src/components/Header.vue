8ed155d5f12b34d1ffced1abecf376ec125cfc2a
arcbjorn <arky.happ@gmail.com>
2022-10-11 13:01:41 -0300

    feat(Header): add company logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CompanyLogo from '@/components/CompanyLogo.vue';
+</script>
 
 <template>
-  <div class="flex justify-between gap-4 flex-wrap p-12">
-    <a href="/" class="cursor-pointer text-xl font-plex-mono font-semibold">
-      arcbjorn
+  <div class="flex justify-between gap-4 flex-wrap py-12 px-8">
+    <a
+      href="https://fundraiseup.com"
+      class="cursor-pointer text-xl font-plex-mono font-semibold"
+    >
+      <CompanyLogo />
     </a>
     <div class="text-lg font-plex-sans">tax calculator</div>
   </div>
